aca11f2a3e493939df0d6433a7eddcb0ad09f2ef
Narasimman Loganathan <naloganathan@altimetrik.com>
2025-06-06 11:39:39 +0530

    my first program

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("hello dude")
+print("hello dude2025")
+
+if 5>2:
+    print("5 is greater than 2")
+
+x=5
+y="string"
+
+print(x)
+print(y)
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
